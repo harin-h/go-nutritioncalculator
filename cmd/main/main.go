ec70fea4c704a7791864bab7e2c9ccfd72f232dc
Good <good.harisombut@gmail.com>
2023-10-30 23:23:42 +0700

    [Good] final

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -17,5 +18,9 @@ func main() {
 	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
 	credentialsOk := handlers.AllowCredentials()
 	routes.RegisterBookStoreRoutes(r)
-	log.Fatal(http.ListenAndServe(":3030", handlers.CORS(originsOk, headersOk, methodsOk, credentialsOk)(r)))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(originsOk, headersOk, methodsOk, credentialsOk)(r)))
 }
